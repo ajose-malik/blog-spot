b2df359242062b270bfccd065da95209ae7e64b9
ajose-malik <ajose.malik@gmail.com>
2022-09-01 18:18:52 -0400

    add event route in posts and comments service

--- comments/index.js
+++ comments/index.js
@@ -13,7 +13,7 @@ app.get('/posts/:id/comments', (req, res) => {
 	res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
 	const commentId = randomBytes(4).toString('hex')
 	const { content } = req.body
 
@@ -22,7 +22,22 @@ app.post('/posts/:id/comments', (req, res) => {
 	comments.push({ id: commentId, content })
 	commentsByPostId[req.params.id] = comments
 
+	await axios.post('http://localhost:4005/events', {
+		type: 'CommentCreated',
+		data: {
+			id: commentId,
+			content,
+			postId: req.params.id
+		}
+	})
+
 	res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+	console.log('Recieved Event', req.body.type)
+
+	res.send({})
 })
 
 app.listen(4001, () => console.log('STARTED 4001'))
